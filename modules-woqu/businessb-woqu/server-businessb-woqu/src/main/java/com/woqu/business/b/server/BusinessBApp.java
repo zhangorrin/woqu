9ba6a8f6e23d555f54eb3a46f7c3a63606c6ff42
orrin <Orrin2008@126.com>
2018-11-19 16:09:19 +0800

    提交图片,去除重复依赖

--- modules-woqu/businessb-woqu/server-businessb-woqu/src/main/java/com/woqu/business/b/server/BusinessBApp.java
+++ modules-woqu/businessb-woqu/server-businessb-woqu/src/main/java/com/woqu/business/b/server/BusinessBApp.java
@@ -3,9 +3,12 @@ package com.woqu.business.b.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author orrin on 2018/7/4
@@ -20,4 +23,11 @@ public class BusinessBApp {
     public static void main(String[] args) {
         SpringApplication.run(BusinessBApp.class, args);
     }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
--- modules-woqu/businessb-woqu/server-businessb-woqu/src/main/java/com/woqu/business/b/server/ConsumeAdditionController.java
+++ modules-woqu/businessb-woqu/server-businessb-woqu/src/main/java/com/woqu/business/b/server/ConsumeAdditionController.java
@@ -5,8 +5,12 @@ import com.woqu.business.b.client.Multiply;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author orrin on 2018/7/4
@@ -23,8 +27,46 @@ public class ConsumeAdditionController implements Multiply {
         return x * y;
     }
 
+    @GetMapping("/area")
     public Integer cuboidArea(@RequestParam("length") int length, @RequestParam("width") int width, @RequestParam("heigh") int heigh) {
         LOGGER.info("length = {}, width = {}, heigh = {}");
         return  this.multiply(2, a.add(a.add(this.multiply(length, width), this.multiply(width, heigh)), this.multiply(length, heigh)));
+    }
+
+    @GetMapping("/area/2")
+    public Integer cuboidArea2(@RequestParam("length") int length, @RequestParam("width") int width, @RequestParam("heigh") int heigh) {
+        LOGGER.info("length = {}, width = {}, heigh = {}");
+        return  this.multiply(2, this.add2(this.add2(this.multiply(length, width), this.multiply(width, heigh)), this.multiply(length, heigh)));
+    }
+
+    /**
+     * 使用 LoadBalancerClient 消费business-a-woqu中的方法
+     */
+    private Integer add2(int x, int y) {
+        ServiceInstance instance = client.choose("business-a-woqu");
+        String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/add?x=" + x + "&y=" + y;
+        return restTemplate.getForObject(url, Integer.class);
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient client;
+
+
+    /**
+     * 使用 Ribbon 消费business-a-woqu中的方法
+     */
+    private Integer add3(int x, int y) {
+        ServiceInstance instance = client.choose("business-a-woqu");
+        String url = "http://business-a-woqu/add?x=" + x + "&y=" + y;
+        return restTemplate.getForObject(url, Integer.class);
+    }
+
+    @GetMapping("/area/3")
+    public Integer cuboidArea3(@RequestParam("length") int length, @RequestParam("width") int width, @RequestParam("heigh") int heigh) {
+        LOGGER.info("length = {}, width = {}, heigh = {}");
+        return  this.multiply(2, this.add3(this.add3(this.multiply(length, width), this.multiply(width, heigh)), this.multiply(length, heigh)));
     }
 }
