675cd98235d3b9a0a1b56a741c803f7d25373a69
orrin <Orrin2008@126.com>
2019-07-09 14:28:40 +0800

    添加prometheus监控

--- common-server-woqu/gateway-woqu/src/main/java/com/woqu/common/gateway/GatewayApp.java
+++ common-server-woqu/gateway-woqu/src/main/java/com/woqu/common/gateway/GatewayApp.java
@@ -1,10 +1,14 @@
 package com.woqu.common.gateway;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -18,5 +22,11 @@ import org.springframework.context.annotation.ComponentScan;
 public class GatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
     }
 }
